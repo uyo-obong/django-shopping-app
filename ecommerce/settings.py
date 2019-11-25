e88652ee5b66275741ae315a36c9ac6d9ea2a106
uyoobonga <uyoobonga@gmail.com>
2019-11-25 22:22:43 +0100

    modified some files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,3 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
